58f65a3a362bdee35c5bbede71b0810155b9af5e
fiabot <fiona.shyne@gmail.com>
2017-06-27 09:43:30 -0500

    Update CommandCenter.py

--- src/CommandCenter.py
+++ src/CommandCenter.py
@@ -1,4 +1,9 @@
+#Authors: Fiona Shyne and London Lowmanstone
+
 import subprocess
+
+#constants
+TESTING = True
 
 class CommandCenter():
     exDir = "~/rsn/umn-ros-pkg/rsn/carpMonitoring/scripts_cmd" #directory to excecute commands from
@@ -8,13 +13,13 @@ class CommandCenter():
         setCommands = {"rudder":"steer_cmd.sh", "propeller":"prop_cmd.sh"}
         
         '''excecutes a command in the shell of the computer to do something
-        If what is "rudder" or "propeller", then *values should be [value to set the thing to, comment aka how much you want it to print]
+        If the parameter what is "rudder" or "propeller", then values should be [value to set the thing to, comment aka how much you want it to print]
         '''
         if what in setCommands:
             #I could make this a lot fewer lines, but then if it throws an error we have very little idea of what went wrong
             value = values[0]
-            if len(values)==2:
-                comment=values[1]
+            if len(values) == 2:
+                comment = values[1]
             else:
                 comment=1 #default
             command = "{} {}".format(setCommands[what], value)
@@ -30,7 +35,12 @@ class CommandCenter():
     def excecuteCommand(command, comment=0):
         if comment==1:
             print("Running command '{}'".format(command))
-        output = subprocess.check_output([command], shell=True)
+        
+        if not TESTING:
+            output = subprocess.check_output([command], shell=True)
+        else:
+            output = "You are testing; no command was actually run."
+            
         if comment==2:
             print("Output of the command was: '{}'".format(output))
         return output
