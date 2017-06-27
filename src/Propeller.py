e4f6418641fcdc678e5542892e72f6434cbfdb37
fiabot <fiona.shyne@gmail.com>
2017-06-27 09:44:29 -0500

    Update Propeller.py

--- src/Propeller.py
+++ src/Propeller.py
@@ -1,13 +1,23 @@
+c#Authors: Fiona Shyne and London Lowmanstone
+
 class Propeller:
-    def __init__(self, max_throttle, min_throttle):
+    def __init__(self,min_throttle, max_throttle, command_center):
+        self.command_center = command_center
         self.max_throttle = max_throttle
         self.min_throttle = min_throttle
         self.on = False
         self.throttle = min_throttle
-    def set_throttle(self, throttle):
-        if throttle == self.min_throttle:
+        
+    def set_throttle(self, throttle, comment=0):
+        if throttle == 0:
             self.on = False
         else:
             self.on = True 
-        print("setting throttle to {}" .format(throttle))
+        if throttle > self.max_throttle:
+            throttle = self.max_throttle
+        elif throttle < self.min_throttle: 
+            throttle = self.min_throttle
+        if comment==1:
+            print("setting throttle to {}" .format(throttle))
+        self.command_center.do("propeller", throttle)
         self.throttle = throttle
