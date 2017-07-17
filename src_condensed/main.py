245aa16a34d0f06bb80f2620b9c32f3efd6e94fb
fiabot <fiona.shyne@gmail.com>
2017-07-17 09:17:00 -0500

    Update main.py

--- src_condensed/main.py
+++ src_condensed/main.py
@@ -11,3 +11,18 @@ OBJ_WIDTH = 40 #mm
 
 boat = boatFunctions(MIN_ANGLE,MAX_ANGLE,MIN_THROTTLE,MAX_THROTTLE)
 vision = Vision(CAM_PORT,FOCAL_LENGTH,OBJ_WIDTH)
+
+initial_flag = 1
+while not rospy.is_shutdown():
+	if initial_flag==1:
+		set_speed(0)
+		initial_flag = 0
+	else:	
+		is_left, distance = vision.find_object_posistion()
+    if is_left is None: 
+      print("can't find ball") 
+    else: 
+      
+      
+     
+	rate.sleep()# the Rate instance keeps the loop at 10hz (which was set  above using "r = rospy.Rate(10)")
